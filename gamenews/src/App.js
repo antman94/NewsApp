eb6da392b2fc0e876a654cdc5f3837531803f150
Ante <anthel@github.com>
2020-12-03 15:29:32 +0100

    Created card component, added styled components for white text and app container and images for the cards.

--- gamenews/src/App.js
+++ gamenews/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import GameCard from './components/GameCard';
+import {
+  AppContainer,
+  TextTEST
+} from './components/common/index.styled';
+import placeholder from './images/placeholder2.jpg';
 
 function App() {
+  
+  const content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <GameCard title='Cyberpunk 2077' poster={placeholder} content={content}/>
+    </AppContainer>
+    
   );
 }
 
---
+++ gamenews/src/components/common/index.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { 
+  Typography
+} from '@material-ui/core';
+
+export const AppContainer = styled.div`
+  width: 100%;
+  min-height: 100%;
+  background-color: lightblue;
+`
+export const GeneralWhiteText = styled(Typography)`
+  color: #FFFFFF;
+  font-size: 14px;
+  font-family: 'Lato';
+`
