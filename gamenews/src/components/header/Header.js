8255daf2226cc38fa7219a2759355e471e6daf56
leticelvin <leticelvin@gmail.com>
2020-12-04 14:07:22 +0100

    almost finished modal

--- gamenews/src/components/header/Header.js
+++ gamenews/src/components/header/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import iconLogo from './icon-fox.png';
+import { useState } from 'react'
+import Modal from 'react-modal';
+import './Header.css'
+
 
 const Header = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
     return (
         <header>
             <img src={iconLogo} alt='icon of a fox' style={{ height: '95px' }} />
@@ -10,9 +15,28 @@ const Header = () => {
                 <p>– I'VE GOT NEWS FOR YA! –</p>
             </div>
             <div className='items-right'>
-                <p>Sign in</p>
+            <button onClick={() => setModalIsOpen(true)}>Sign in</button>
+                <Modal className="signinmodal" isOpen={modalIsOpen}>
+                    <h1 className="signintext">SIGN IN</h1>
+                    <button onClick={() => setModalIsOpen(false)}>Close</button><br />
+                    <label for="nameinput">NAME</label><br />
+                    <input type="text" className="nameinput"></input><br />
+
+                    <label for="nameinput">USERNAME</label><br />
+                    <input type="text" className="nameinput"></input><br />
+
+                    <label for="nameinput">PASSWORD</label><br />
+                    <input type="text" className="nameinput"></input><br />
+
+                    <label for="nameinput">CONFIRM PASSWORD</label><br />
+                    <input type="text" className="nameinput"></input><br />
+
+                    <h3 className="bottomtext">Already have an account?</h3>
+                    <h3 className="linktologin">Click here</h3>
+                </Modal>
                 <p>Login</p>
             </div>
+
         </header>
     )
 }
