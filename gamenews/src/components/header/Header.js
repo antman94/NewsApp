c4b8356abda986afd2432f8c6af54221b228a7a8
leticelvin <leticelvin@gmail.com>
2020-12-05 19:39:11 +0100

    Finished

--- gamenews/src/components/header/Header.js
+++ gamenews/src/components/header/Header.js
@@ -18,8 +18,8 @@ const Header = () => {
             <div className='items-right'>
             <button className="signinmodalbutton" onClick={() => setModalIsOpen(true)}><h3>SIGN IN</h3></button>
                 <Modal className="signinmodal" isOpen={modalIsOpen}>
-                    <h1 className="signintext">SIGN IN</h1>
                     <button className="closebutton" onClick={() => setModalIsOpen(false)}>Close</button><br />
+                    <h1 className="signintext">SIGN IN</h1>
                     <label for="nameinput">NAME</label><br />
                     <input type="text" className="nameinput"></input><br />
 
@@ -36,7 +36,7 @@ const Header = () => {
                     <h3 className="linktologin">Click here</h3>
                     <button className="signinbutton">Sign In</button>
                 </Modal>
-                <button className="loginmodalbutton" onClick={() => setLoginModalIsOpen(true)}><p>Login</p></button>
+                <button className="loginmodalbutton" onClick={() => setLoginModalIsOpen(true)}><h3>LOGIN</h3></button>
 
 		        <Modal className="loginmodal" isOpen={loginmodalIsOpen}>
 			        <button className="loginclosebutton" onClick={() => setLoginModalIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></button>
@@ -49,6 +49,7 @@ const Header = () => {
 
                     <h3 className="bottomtext">Already have an account?</h3>
                     <h3 className="linktologin">Click here</h3>
+                    <button className="loginbutton">Sign In</button>
 		        </Modal>
             </div>
 
