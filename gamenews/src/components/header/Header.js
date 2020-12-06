c823a41bc80a9957b33eee732b35b4b12dba934a
Carolina Arvidsson <carolinaarvidsson_@live.se>
2020-12-06 23:49:01 +0100

    finished header

--- gamenews/src/components/header/Header.js
+++ gamenews/src/components/header/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import iconLogo from './icon-fox.png';
+import DayJS from 'react-dayjs';
+
+const currentDate = '';
 
 const Header = () => {
+
     return (
         <header>
-            <img src={iconLogo} alt='icon of a fox' style={{ height: '95px' }} />
-            <div className='column-container'>
-                <h1>Game Over News</h1>
-                <p>– I'VE GOT NEWS FOR YA! –</p>
+            <div className='items-center'>
+                <img src={iconLogo} alt='icon of a fox' className='logo' style={{ height: '100px' }} />
+                <div className='column-container'>
+                    <h1>Ga<span className='m'>m</span>e Ove<span className='r'>r</span> News</h1>
+                    <DayJS className='date' format='dddd D MMMM YYYY'>{ currentDate }</DayJS>
+                </div>
             </div>
             <div className='items-right'>
-                <p>Sign in</p>
-                <p>Login</p>
+                <button className='login-btn'>Login</button>
             </div>
         </header>
     )
