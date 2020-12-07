03f2c72a6a643e30e916c0bd8bf88618265a1408
elinwikefors <elinwikefors98@gmail.com>
2020-12-07 11:23:59 +0100

    Merged with main

--- gamenews/src/App.js
+++ gamenews/src/App.js
@@ -7,20 +7,12 @@ import Leftmenu from './components/LeftMenuComponent/LeftMenu';
 function App() {
 
   return (
-<<<<<<< HEAD
-    <AppContainer>
-      <CardComponentContainer />
-      <Leftmenu />
-    </AppContainer>
-    
-=======
     <div className="App">
       <Header />
       <AppContainer>
         <CardComponentContainer />
       </AppContainer>
     </div>
->>>>>>> 5836caed14b18c8b762a25a6a0610a142ec67cd3
   );
 }
 
