c3fde263097cb448b4cb84db7bdaaebcfdf53073
leticelvin <leticelvin@gmail.com>
2020-12-09 16:44:07 +0100

    Removed white barrier

--- gamenews/src/components/header/Header.js
+++ gamenews/src/components/header/Header.js
@@ -3,13 +3,12 @@ import iconLogo from './icon-fox.png';
 import { useState } from 'react'
 import Modal from 'react-modal';
 import './Header.css'
-
+import DayJS from 'react-dayjs';
 
 const Header = () => {
 
     const [modalIsOpen, setModalIsOpen] = useState(false)
     const [loginmodalIsOpen, setLoginModalIsOpen] = useState(false)
-import DayJS from 'react-dayjs';
 
 const currentDate = '';
 
@@ -22,7 +21,7 @@ const currentDate = '';
                     <DayJS className='date' format='dddd D MMMM YYYY'>{ currentDate }</DayJS>
                 </div>
             </div>
-            <div className='items-right'>
+            <div className='items-right'></div>
             <button className="signinmodalbutton" onClick={() => setModalIsOpen(true)}><h3>SIGN UP</h3></button>
                 <Modal className="signinmodal" isOpen={modalIsOpen}>
                     <button className="closebutton" onClick={() => setModalIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></button><br />
@@ -58,10 +57,9 @@ const currentDate = '';
                     <button className="loginbutton">Sign In</button>
 		        </Modal>
                 <button className='login-btn'>Login</button>
-            </div>
 
         </header>
     )
 }
 
-export default Header();
+export default Header;
