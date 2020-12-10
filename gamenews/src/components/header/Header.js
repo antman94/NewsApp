6b7e39261ee5a3f8292790c02111898b5325d288
leticelvin <leticelvin@gmail.com>
2020-12-10 10:12:09 +0100

    Fixed modal position and login signin buttons

--- gamenews/src/components/header/Header.js
+++ gamenews/src/components/header/Header.js
@@ -21,7 +21,7 @@ const currentDate = '';
                     <DayJS className='date' format='dddd D MMMM YYYY'>{ currentDate }</DayJS>
                 </div>
             </div>
-            <div className='items-right'></div>
+            <div className='items-right'>
             <button className="signinmodalbutton" onClick={() => setModalIsOpen(true)}><h3>SIGN UP</h3></button>
                 <Modal className="signinmodal" isOpen={modalIsOpen}>
                     <button className="closebutton" onClick={() => setModalIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></button><br />
@@ -42,7 +42,6 @@ const currentDate = '';
                     <h3 className="linktologin">Click here</h3>
                     <button className="signinbutton">Sign In</button>
                 </Modal>
-                <button className="loginmodalbutton" onClick={() => setLoginModalIsOpen(true)}><h3>LOGIN</h3></button>
 
 		        <Modal className="loginmodal" isOpen={loginmodalIsOpen}>
 			        <button className="loginclosebutton" onClick={() => setLoginModalIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></button>
@@ -56,7 +55,8 @@ const currentDate = '';
                     <h3 className="bottomtext">Already have an account?<span className="linktologin">Click Here</span></h3>
                     <button className="loginbutton">Sign In</button>
 		        </Modal>
-                <button className='login-btn'>Login</button>
+                <button className='login-btn' onClick={() => setLoginModalIsOpen(true)}>Login</button>
+                </div>
 
         </header>
     )
