bd31efcfa774989926aaadd13c3a20ff1f9bba8d
leticelvin <leticelvin@gmail.com>
2020-12-10 15:25:39 +0100

    Removed signin modal

--- gamenews/src/components/header/Header.js
+++ gamenews/src/components/header/Header.js
@@ -7,10 +7,9 @@ import DayJS from 'react-dayjs';
 
 const Header = () => {
 
-    const [modalIsOpen, setModalIsOpen] = useState(false)
     const [loginmodalIsOpen, setLoginModalIsOpen] = useState(false)
 
-const currentDate = '';
+    const currentDate = '';
 
     return (
         <header>
@@ -22,26 +21,8 @@ const currentDate = '';
                 </div>
             </div>
             <div className='items-right'>
-            <button className="signinmodalbutton" onClick={() => setModalIsOpen(true)}><h3>SIGN UP</h3></button>
-                <Modal className="signinmodal" isOpen={modalIsOpen}>
-                    <button className="closebutton" onClick={() => setModalIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></button><br />
-                    <h1 className="signintext">SIGN UP</h1>
-                    <label for="nameinput">NAME</label><br />
-                    <input type="text" className="nameinput"></input><br />
 
-                    <label for="nameinput">USERNAME</label><br />
-                    <input type="text" className="nameinput"></input><br />
-
-                    <label for="nameinput">PASSWORD</label><br />
-                    <input type="text" className="nameinput"></input><br />
-
-                    <label for="nameinput">CONFIRM PASSWORD</label><br />
-                    <input type="text" className="nameinput"></input><br />
-
-                    <h3 className="bottomtext">Already have an account?</h3>
-                    <h3 className="linktologin">Click here</h3>
-                    <button className="signinbutton">Sign In</button>
-                </Modal>
+            <button className='login-btn' onClick={() => setLoginModalIsOpen(true)}>Login</button>
 
 		        <Modal className="loginmodal" isOpen={loginmodalIsOpen}>
 			        <button className="loginclosebutton" onClick={() => setLoginModalIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></button>
@@ -52,10 +33,9 @@ const currentDate = '';
                     <label for="loginnameinput">PASSWORD</label><br />
                     <input type="text" className="nameinput"></input><br />
 
-                    <h3 className="bottomtext">Already have an account?<span className="linktologin">Click Here</span></h3>
+                    <h3 className="bottomtext">Don't have an account?<span className="linktologin">Click Here</span></h3>
                     <button className="loginbutton">Sign In</button>
 		        </Modal>
-                <button className='login-btn' onClick={() => setLoginModalIsOpen(true)}>Login</button>
                 </div>
 
         </header>
