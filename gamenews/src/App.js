1b3d848501b515807fe887ddee38b007a84bd6ee
elinwikefors <elinwikefors98@gmail.com>
2020-12-15 16:13:22 +0100

    merged with main

--- gamenews/src/App.js
+++ gamenews/src/App.js
@@ -2,32 +2,20 @@ import './App.scss';
 import Header from './components/header/Header';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CardComponentContainer from './containers/CardComponentContainer';
-<<<<<<< HEAD
-import GameDetails from './components/GameDetails/GameDetails';
-=======
 import GameInfoContainer from './containers/GameInfoContainer/GameInfoContainer';
 import Leftmenu from './components/LeftMenuComponent/LeftMenu';
->>>>>>> c912d4e031a9b1d36505aa7086d3546b148d9623
 
 function App() {
 
   return (
     <Router>
       <Header />
-<<<<<<< HEAD
-      <AppContainer>
-        {/* <CardComponentContainer /> */}
-        <GameDetails />
-      </AppContainer>
-    </div>
-=======
       <Leftmenu />
         <Switch>
           <Route path="/" exact component={CardComponentContainer}/>
           <Route path="/gameInfo/:gameId" component={GameInfoContainer}/>
         </Switch>  
     </Router>
->>>>>>> c912d4e031a9b1d36505aa7086d3546b148d9623
   );
 }
 
