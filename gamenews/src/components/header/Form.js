10c83e137c521591425aed290cbe7269a4a2782e
leticelvin <leticelvin@gmail.com>
2020-12-18 13:07:31 +0100

    Finished Validation

--- gamenews/src/components/header/Form.js
+++ gamenews/src/components/header/Form.js
@@ -8,19 +8,21 @@ class Form extends React.Component {
         this.state = {
         username: "",
         password: "",
+        nameError: "",
+        passwordError: "",
         }
         
     }
     /*-------------------------------------------------------------------------------------------*/ 
-    formValid = (formErrors) => {
-        let valid = true;
-      
-        Object.values(formErrors).forEach(val => {
-        val.length > 0 && (valid = false);
-    });
+        /*formValid = (formErrors) => {
+            let valid = true;
+        
+            Object.values(formErrors).forEach(val => {
+            val.length > 0 && (valid = false);
+        });
 
-    return valid;
-    };
+        return valid;
+        };*/
     
     /*------------------------------------------------------------------------------------------*/ 
 
@@ -28,26 +30,48 @@ class Form extends React.Component {
 
         validate = () => {
             let nameError = "";
+            let passwordError = "";
 
             if (!this.state.username) {
                 nameError = "Name cannot be blank";
+                console.clear();
+                console.log("Username cannot be blank");
+            }
+
+            if (!this.state.password) {
+                passwordError = "Password cannot be blank";
+                console.log("Password cannot be blank");
+            }
+
+            if (this.state.password.length < 3) {
+                alert("Password is too short. Please choose a different password.");
+                return false;
             }
 
             if (nameError) {
                 return false;
             }
+
+            if (passwordError) {
+                return false;
+            }
+
         }
 
     /*-------------------------------------------------------------------------------------*/ 
 
         /*handlesubmit checks if the credentials in the formErrors object are correct*/ 
-        handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e.target.value);
+        handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({username: event.target.value});
+        this.setState({password: event.target.value})
+        console.clear();
+        console.log(this.state.username);
+        console.log(this.state.password);
         
         const isValid = this.validate();
         if (isValid) {
-            console.log(this.state);
+            console.log("validate");
         }
         /*const { name, value } = e.target;
 
@@ -56,7 +80,18 @@ class Form extends React.Component {
                 "First name: " 
             );
         } else { console.error("Form invalid") }*/
+
+        /*value={this.state.username}*/
         };
+
+    /*------------------------------------------------------------------------------------------*/
+        handleUsernameChange = (event) => {
+            this.setState({username: event.target.value})
+        }
+
+        handlePasswordChange = (event) => {
+            this.setState({password: event.target.value})
+        }
 
     /*------------------------------------------------------------------------------------------*/
 
@@ -65,10 +100,10 @@ class Form extends React.Component {
                     <h1 className="logintext">LOGIN</h1>
 
                     <label for="loginnameinput">USERNAME</label><br />
-                    <input type="text" className="nameinput"></input><br />
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange} className="nameinput"></input><br />
 
                     <label for="loginnameinput">PASSWORD</label><br />
-                    <input type="password" className="nameinput"></input><br />
+                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} className="nameinput"></input><br />
 
                     <h3 className="bottomtext">Don't have an account?<span className="linktologin">Click Here</span></h3>
                     <button type="submit" className="loginbutton">Sign In</button>
